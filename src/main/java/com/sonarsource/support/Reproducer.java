c7aa06edbbe2d59406e30b836896820b08a6ce65
Eric Le Goff <eric.legoff@sonarsource.com>
2021-06-24 10:48:50 +0200

    add method with no comments

--- src/main/java/com/sonarsource/support/Reproducer.java
+++ src/main/java/com/sonarsource/support/Reproducer.java
@@ -1,13 +1,29 @@
 package com.sonarsource.support;
 
-    //comment 1
-    public class Reproducer {
+//comment 1
+public class Reproducer {
     public static void main(String[] args) {
         //comment 2
-       String foo = "foo";
+        String foo = "foo";
         if (foo.length() == 3) {
             System.exit(0);
+        }else {
+            addMethodWithMissingComments()
         }
+    }
+
+
+    public static void addMethodWithMissingComments(){
+        int a = 1;
+        int b= 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+
+        if (a + b +c + d +e == 42) {
+            System.exit(0);
+        }
+
     }
 }
     
